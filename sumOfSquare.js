b2a5bbdf35bb9f8c2f492f32e28acad2f61e75fb
Thuan Truong <21116370+NewT74@users.noreply.github.com>
2020-10-07 11:33:50 +0700

    Create sumOfSquare.js

---
+++ sumOfSquare.js
@@ -0,0 +1,10 @@
+function sumOfSquare(n) {
+    let ans = 0;
+    for(let c = Math.floor(Math.sqrt(n)); c >= 1; c--) {
+	    for(let b = Math.floor(Math.sqrt(n - Math.pow(c, 2))); b >= 0; b--) {
+		    let a = Math.floor(Math.sqrt(n - Math.pow(b, 2) - Math.pow(c, 2)));
+		    if(Math.pow(a, 2) + Math.pow(b, 2) + Math.pow(c, 2) == n && a < b && b < c) ans++;
+	    }
+    }
+    return ans;
+}
